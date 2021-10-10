28ec5df07a075221cf69710968dc8bc3de9d0082
tk-rusch <konstantin.rusch@sam.math.ethz.ch>
2021-10-10 12:12:04 +0200

    added psMNIST

---
+++ psMNIST/network.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import math
+
+class LEMCell(nn.Module):
+    def __init__(self, ninp, nhid, dt):
+        super(LEMCell, self).__init__()
+        self.ninp = ninp
+        self.nhid = nhid
+        self.dt = dt
+        self.inp2hid = nn.Linear(ninp, 4 * nhid)
+        self.hid2hid = nn.Linear(nhid, 3 * nhid)
+        self.transform_z = nn.Linear(nhid, nhid)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        std = 1.0 / math.sqrt(self.nhid)
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
+
+    def forward(self, x, y, z):
+        transformed_inp = self.inp2hid(x)
+        transformed_hid = self.hid2hid(y)
+        i_dt1, i_dt2, i_z, i_y = transformed_inp.chunk(4, 1)
+        h_dt1, h_dt2, h_y = transformed_hid.chunk(3, 1)
+
+        ms_dt_bar = self.dt * torch.sigmoid(i_dt1 + h_dt1)
+        ms_dt = self.dt * torch.sigmoid(i_dt2 + h_dt2)
+
+        z = (1.-ms_dt) * z + ms_dt * torch.tanh(i_y + h_y)
+        y = (1.-ms_dt_bar)* y + ms_dt_bar * torch.tanh(self.transform_z(z)+i_z)
+
+        return y, z
+
+class LEM(nn.Module):
+    def __init__(self, ninp, nhid, nout, dt=1.):
+        super(LEM, self).__init__()
+        self.nhid = nhid
+        self.cell = LEMCell(ninp,nhid,dt)
+        self.classifier = nn.Linear(nhid, nout)
+        self.init_weights()
+
+    def init_weights(self):
+        for name, param in self.named_parameters():
+            if 'classifier' in name and 'weight' in name:
+                nn.init.kaiming_normal_(param.data)
+
+    def forward(self, input):
+        ## initialize hidden states
+        y = input.data.new(input.size(1), self.nhid).zero_()
+        z = input.data.new(input.size(1), self.nhid).zero_()
+        for x in input:
+            y, z = self.cell(x,y,z)
+        out = self.classifier(y)
+        return out
---
+++ psMNIST/utils.py
@@ -0,0 +1,30 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+def get_data(bs_train,bs_test):
+    train_dataset = torchvision.datasets.MNIST(root='data/',
+                                               train=True,
+                                               transform=transforms.ToTensor(),
+                                               download=True)
+
+    test_dataset = torchvision.datasets.MNIST(root='data/',
+                                              train=False,
+                                              transform=transforms.ToTensor())
+
+    train_dataset, valid_dataset = torch.utils.data.random_split(train_dataset, [57000,3000])
+
+    # Data loader
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=bs_train,
+                                               shuffle=True)
+
+    valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset,
+                                              batch_size=bs_test,
+                                              shuffle=False)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=bs_test,
+                                              shuffle=False)
+
+    return train_loader, valid_loader, test_loader
